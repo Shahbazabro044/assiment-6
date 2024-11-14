a7d9907984ec8af14ada8037a13988c9fca75774
Shahbaz <167298370+Shahbazabro044@users.noreply.github.com>
2024-11-14 15:29:28 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Image from "next/image"
+export default function Home(){
+    return(
+        <div>
+            <img src="Portfolio - Simple Footer.png" alt="website" width={2000}height={2000}/>
+            <img src="About - Simple Footer@2x.png" alt="website" width={2000}height={2000}/>
+            <img src="form.png" alt="website" width={2000}height={2000}/>
+            
+        </div>
+    )
+}
+
+
